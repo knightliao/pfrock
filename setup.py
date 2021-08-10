fd90414362348713297d6b3cad04f299bbe1a7d4
knightliao <knightliao@gmail.com>
2021-08-10 17:19:10 +0800

    安装版本限制控制

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ import io
 import os
 import re
 
-from pip.req import parse_requirements
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
 f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
